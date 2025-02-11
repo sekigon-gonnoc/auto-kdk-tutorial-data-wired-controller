03659c14a55a910b14954db1784fd9b87147cb8c
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2025-02-11 23:33:21 +0900

    Add files via upload

--- qmk_firmware/keyboards/tutorial/keymaps/vial/config.h
+++ qmk_firmware/keyboards/tutorial/keymaps/vial/config.h
@@ -1,1 +1,6 @@
+#pragma once
 #define EE_HANDS
+#define VIAL_KEYBOARD_UID {0xfe, 0x30, 0x03, 0x0e, 0x10, 0x2e, 0xcf, 0x8f}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
